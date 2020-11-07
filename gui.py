c7f9b0787f01e9da0bec8e9d3a9bdc045f2191a7
Huseyin Gokay <45242258+huseyingokay@users.noreply.github.com>
2020-11-07 12:01:41 +0300

    extra parameters added to text

--- gui.py
+++ gui.py
@@ -4,15 +4,18 @@ import fileinput
 from fpdf import FPDF
 
 class Gui:
-    MY_ADDRESS = 'huseyingokay_1999@hotmail.com'
+    SENDER_ADDRESS = 'huseyingokay_1999@hotmail.com'
     RECEIVER_ADDRESS = 'huseyin.gokay@ozu.edu.tr'
-    RECEIVER = "ahmet"
+    RECEIVER = "Hüseyin Bey"
+    SENDER = "Ahmet Boi"
     POSITION = "SOFTWARE"
     PASSWORD = 'XRAYSKINGSTAR8520'
-    MESSAGE = 'denemeedfskljdsfkljdgkldf'
+    MESSAGE = 'My job application letter'
     SUBJECT = 'AMETTEN GELDI'
     ATTACH = 'ornekahmet.pdf'
     OUTPUT_PDF = "interview_letter.pdf"
+    DATE = "August 15, 2020"
+    PHONE = "555-555-4123"
 
     def pdfReadAndWrite(self):
         file = open("ornek.txt", "r+")
@@ -27,8 +30,19 @@ class Gui:
         for x in fileData:
             if("(RECEIVER)" in x):
                 x = x.replace("(RECEIVER)", self.RECEIVER)
+            elif("(SENDER)" in x):
+                x = x.replace("(SENDER)", self.SENDER)
             elif("(POSITION)" in x):
                 x = x.replace("(POSITION)", self.POSITION)
+            elif("(SENDER_ADDRESS)" in x):
+                x = x.replace("(SENDER_ADDRESS)", self.SENDER_ADDRESS)
+            elif("(RECEIVER_ADDRESS)" in x):
+                x = x.replace("(RECEIVER_ADDRESS)", self.RECEIVER_ADDRESS)
+            elif("(DATE)" in x):
+                x = x.replace("(DATE)", self.DATE)
+            elif("(PHONE)" in x):
+                x = x.replace("(PHONE)", self.PHONE)
+
             pdf.multi_cell(185, 5, txt = x, align = 'L')
         # save the pdf with name .pdf
         pdf.output(self.OUTPUT_PDF)
